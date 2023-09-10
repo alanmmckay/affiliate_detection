b61bb211eeb36b9e0c5870e94add94113fecfb3a
Alan McKay <alanmmckay@gmail.com>
2023-09-10 17:45:33 -0500

    Added more method functionality pertaining to how the sourceinterface may retreive anchor information. This involved some code refactoring in which the new methods are reliant upon get_anchor_soup.

--- helper_functions.py
+++ helper_functions.py
@@ -3,6 +3,7 @@ import json
 import sqlite3 as lite
 from bs4 import BeautifulSoup
 
+
 class DataInterface(object):
     #db_object = DataInterface("./crawl-data.sqlite")
     def __init__(self,path,file_name):
@@ -24,16 +25,19 @@ class DataInterface(object):
             f.write(log_str)
             f.close()
 
+
 #Seems to be an opportunity for a SourcesInterface in addition to this:
 class SourceInterface(object):
     def __init__(self, path = 'datadir/sources/'):
         self._path = path
         self._source_files = os.listdir(path)
+        self._soup = None
 
     def set_html_file(self,file_name):
         f = open(self._path+file_name, 'r')
         self._file_name = file_name
         self._file_data = f.read()
+        self._soup = None
         f.close()
 
     def get_source_files(self):
@@ -42,27 +46,64 @@ class SourceInterface(object):
     def get_visit_id(self):
         return self._file_name.split('-')[0]
 
-    def get_anchor_hrefs(self):
-        soup = BeautifulSoup(self._file_data,features="html.parser")
+    def get_anchor_soup(self):
+        if not self._soup:
+            self._soup = BeautifulSoup(self._file_data,features="html.parser")
         anchors = list()
-        for anchor in soup.findAll('a'):
+        for anchor in self._soup.findAll('a'):
             try:
-                anchors.append(anchor['href'])
+                anchors.append(anchor)
             except:
-                log_str = "Error within page: " + self._file_name + "\n"
-                log_str += 'Tried to access href of the following element: \n'
-                log_str += str(anchor)
-                log_str += '\n\n\n\n'
-                f = open('error_logs/get_anchor_hrefs_log.txt','a')
-                f.write(log_str)
-                f.close()
+                print("error in get_anchor_soup")
         return anchors
 
-    def get_anchor_info(self):
-        #tool a function which grabs *all* anchor info
-        #soup.findAll('x')[i].attrs
-        #soup.findAll('x')[i].contents
-        pass
+    def get_anchor_hrefs(self, soup_anchor_object = None):
+        if not soup_anchor_object:
+            soup = self.get_anchor_soup()
+            anchor_hrefs = list()
+            for anchor in soup:
+                try:
+                    anchor_hrefs.append(anchor['href'])
+                except:
+                    log_str = "Error within page: " + self._file_name + "\n"
+                    log_str += 'Tried to access href of the following element: \n'
+                    log_str += str(anchor)
+                    log_str += '\n\n\n\n'
+                    f = open('error_logs/get_anchor_hrefs_log.txt','a')
+                    f.write(log_str)
+                    f.close()
+            return anchor_hrefs
+        else:
+            return soup_anchor_object['href']
+
+    def get_anchor_attributes(self, soup_anchor_object = None):
+        if not soup_anchor_object:
+            soup = self.get_anchor_soup()
+            anchor_attributes = list()
+            for anchor in soup:
+                attributes = anchor.attrs
+                try:
+                    anchor_attributes.append(attributes)
+                except:
+                    print("error in get_anchor_info")
+            return anchor_attributes
+        else:
+            return soup_anchor_object.attrs
+
+    def get_anchor_content(self, soup_anchor_object = None):
+        if not soup_anchor_object:
+            soup = self.get_anchor_soup()
+            anchor_content = list()
+            for anchor in soup:
+                content = anchor.contents
+                try:
+                    anchor_content.append(content)
+                except:
+                    print("error in get_anchor_content")
+            return anchor_content
+        else:
+            return soup_anchor_object.contents
+
 
 
 class JsonInterface(object):
@@ -84,10 +125,16 @@ class JsonInterface(object):
 
 if __name__ == "__main__":
     #Change this test to ask for filename:
-    test_file_name = '3571936341707252-c001159620014cf3dd33a0f41016184f.html'
+    test_file_name = '68178416070244-783bb746078891ed286fa6d144b7ffd9.html'
     data_object = DataInterface('datadir/','crawl-data.sqlite')
     print(data_object.get_url_from_source(test_file_name))
     source_object = SourceInterface()
     source_object.set_html_file(test_file_name)
     print(source_object.get_visit_id())
+    anchors = source_object.get_anchor_soup()
+    print(source_object.get_anchor_attributes(anchors[0]))
+    print(source_object.get_anchor_hrefs(anchors[0]))
+    print(source_object.get_anchor_content(anchors[0]))
+    #print(source_object.get_anchor_attributes())
     #print(source_object.get_anchor_hrefs())
+    #print(source_object.get_anchor_content())
