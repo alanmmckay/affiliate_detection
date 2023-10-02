e3030697c792566c4c841a76b286470baaa4a5c1
Alan McKay <alanmmckay@gmail.com>
2023-10-01 21:47:23 -0500

    UI annotator is in a working state. Can still generalize a few items such as filenames...

--- annotate.py
+++ annotate.py
@@ -1,10 +1,18 @@
 from helper_functions import DataInterface, JsonInterface, SourceInterface
+from annotation_appender import append_script
 from urllib.parse import urlparse
 import os
+import sys
 
 json_handler = JsonInterface()
 info = json_handler.read_from_file('analysis_files/info.json')
 db_handler = DataInterface("datadir/","/crawl-data.sqlite")
+
+mode = input("Input Mode: ")
+
+if not (mode == "ui" or mode =="manual"):
+    print("Please enter valid input mode.")
+    sys.exit()
 
 inputStr = str()
 quit = ['quit','Quit','QUIT']
@@ -22,40 +30,61 @@ while inputStr not in quit:
 
         local_page = db_handler.get_source_from_url(inputStr)
 
-        os.system('firefox ' + 'datadir/sources/' + local_page + ' > /dev/null 2>&1 &')
-
         for anchor in info[hostname][url]['anchors']:
             if urlparse(anchor).hostname in whitelist:
                 info[hostname][url]['anchors'][anchor]['annotation'] = 'yes'
                 print(str(anchor) + " annotation switched to yes")
 
         print("Local filename: " + local_page)
-        inputStr = input("Input anchor URL to annotate: ")
 
-        while inputStr not in cont:
-
-            anchor = inputStr
-
-            if anchor in info[hostname][url]['anchors']:
-
-                inputStr = input("Annotation value: ")
-
-                if inputStr != None:
-                    old_val = info[hostname][url]['anchors'][anchor]['annotation']
-                    info[hostname][url]['anchors'][anchor]['annotation'] = inputStr
-                    print(str(anchor) + " annotation switched from " + str(old_val) + " to " + inputStr)
-
-            else:
-
-                print(" !!! Invalid anchor URL: ")
-                print(anchor)
+        if mode == "manual":
+            os.system('firefox ' + 'datadir/sources/' + local_page + ' > /dev/null 2>&1 &')
 
             inputStr = input("Input anchor URL to annotate: ")
+            while inputStr not in cont:
+
+                anchor = inputStr
+                if anchor in info[hostname][url]['anchors']:
+
+                    inputStr = input("Annotation value: ")
+                    if inputStr != None:
+
+                        old_val = info[hostname][url]['anchors'][anchor]['annotation']
+                        info[hostname][url]['anchors'][anchor]['annotation'] = inputStr
+                        print(str(anchor) + " annotation switched from " + str(old_val) + " to " + inputStr)
+
+                else:
+                    print(" !!! Invalid anchor URL: ")
+                    print(anchor)
+
+                inputStr = input("Input anchor URL to annotate: ")
+
+            json_handler.write_to_file('analysis_files/info.json',info)
+            os.system('firefoxPID=$(pgrep firefox); kill $firefoxPID;')
+
+        elif mode == "ui":
+            append_script('datadir/sources/'+local_page, url, "links.txt", "temp.html")
+
+            os.system('firefox temp.html > /dev/null 2>&1 &')
+
+            inputStr = input("Input 'continue' once links have been selected: ")
+
+            while inputStr != "continue":
+                inputStr = input("Input 'continue' once links have been selected: ")
 
 
-        json_handler.write_to_file('analysis_files/info.json',info)
+            f = open('links.txt','r')
+            link_listing = f.readlines()
+            f.close()
 
-        os.system('firefoxPID=$(pgrep firefox); kill $firefoxPID;')
+            for link in link_listing:
+                anchor = link.split()[0]
+                old_val = info[hostname][url]['anchors'][anchor]['annotation']
+                info[hostname][url]['anchors'][anchor]['annotation'] = 'yes'
+                print(str(anchor) + " annotation switched from " + str(old_val) + " to " + 'yes')
+
+            json_handler.write_to_file('analysis_files/info.json',info)
+            os.system('firefoxPID=$(pgrep firefox); kill $firefoxPID;')
 
     else:
 
